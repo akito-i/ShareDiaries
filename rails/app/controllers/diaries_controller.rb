197bd4dab0ffa67ed208089d27e91683a0f77b0a
akito-i <a.imamura0827@gmail.com>
2021-10-02 19:38:28 +0900

    integration_test #6
    railsとnuxtの疎通確認用の変更

---
+++ rails/app/controllers/diaries_controller.rb
@@ -0,0 +1,10 @@
+class DiariesController < ApplicationController
+  def index
+    if Diary.count.zero?
+      ['1st', '2nd', '3rd'].each do |title|
+        Diary.create!(title: title)
+      end
+    end
+      render json: { diaries: Diary.all.to_json }
+    end
+  end
