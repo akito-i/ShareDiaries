8afd5ffa952638046bc5e967b3ef3587f1b4f823
akito-i <a.imamura0827@gmail.com>
2021-10-03 14:26:08 +0900

    login_page_create #14
    パスワードの表示・非表示切り替え機能も同時に作成

---
+++ nuxt/pages/login/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h2>
+          ログイン
+        </h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" lazy-validation>
+          <v-text-field
+            v-model="email"
+            prepend-icon="mdi-email"
+            label="メールアドレス"
+          />
+          <v-text-field
+            v-model="password"
+            prepend-icon="mdi-lock"
+            :append-icon="toggle.icon"
+            :type="toggle.type"
+            autocomplete=“on”
+            @click:append="show = !show"
+            label="パスワード"
+          />
+          <v-card-actions>
+            <v-btn
+              color="light-green"
+              class="white--text"
+              @click="login"
+            >
+              ログイン
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      show: false
+    }
+  },
+  computed: {
+    toggle () {
+      const icon = this.show ? 'mdi-eye' : 'mdi-eye-off'
+      const type = this.show ? 'text' : 'password'
+      return { icon, type }
+    }
+  }
+}
+</script>
